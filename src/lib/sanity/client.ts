2e4feb86bfa1223a919f286715699fe800237e94
Sean Lennon <lennons301@gmail.com>
2026-04-10 12:55:15 +0100

    fix: remove invalid Sanity client fallback for production builds

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -3,7 +3,7 @@ import imageUrlBuilder from "@sanity/image-url";
 import type { Image } from "sanity";
 
 export const sanityClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "unconfigured",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production",
   apiVersion: "2026-04-09",
   useCdn: process.env.NODE_ENV === "production",
